53f603a9133bd6765ab8d960678e72a6d3d06325
sanskardhumal220-sys <sanskardhumal220@gmail.com>
2025-08-29 14:40:28 +0530

    first commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <nav class="navbar bg-dark navbar-dark navbar-expand-md fixed-top">
+    <div class="container-fluid">
+      <a href="" class="navbar-brand">
+         <img src="public\ff-logo-02.png" alt="" class="weblogo"/>
+      </a>
+      <div class="menu">
+      <ul class="navbar-nav">
+         <li><a href="" class="nav-link">home</a></li>
+         <li><a href="" class="nav-link">about</a></li>
+         <li><a href="" class="nav-link"> brand</a></li>
+         <li><a href="" class="nav-link">our team</a></li>
+         <li><a href="" class="nav-link">contact</a></li>
+      </ul>
+   </div>
+</div>
+   </nav>
+    </div>
+  )
+}
+
+export default Header
